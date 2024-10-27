a15f49c627704618981d098e2586a928986e95f4
Riane <rianefmz@gmail.com>
2024-10-26 21:53:52 -0300

    style: ajusta estilização e layout em todos os arquivos

---
+++ js/cadastro.js
@@ -0,0 +1,11 @@
+let alunos = [];
+
+class aluno {
+    constructor(nome,idade,endereco,responsavel){
+        this.nome = nome;
+        this.idade = idade;
+        this.endereco = endereco;
+        this.responsavel = responsavel;
+    }
+}
+
