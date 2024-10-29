7dd3cc78db8ab3f81c3deef7a23fd6601eaeacea
Riane <rianefmz@gmail.com>
2024-10-28 21:10:22 -0300

    docs: atualiza js

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,7 +1,7 @@
 let alunos = [];
 
-class aluno {
-    constructor(nome,idade,endereco,responsavel){
+class Aluno {
+    constructor(nome, idade, endereco, responsavel) {
         this.nome = nome;
         this.idade = idade;
         this.endereco = endereco;
@@ -9,3 +9,46 @@ class aluno {
     }
 }
 
+function adicionarAlunos() {
+    const nome = document.getElementById("nome").value;
+    const idade = document.getElementById("idade").value;
+    const endereco = document.getElementById("endereco").value;
+    const responsavel = document.getElementById("responsavel").value;
+
+    if (nome && idade && endereco && responsavel) {
+        const alunoNovo = new Aluno(nome, idade, endereco, responsavel);
+        alunos.push(alunoNovo);
+        document.getElementById("form-aluno").reset();
+        showMessage("Aluno cadastrado com sucesso!");
+    } else {
+        showMessage("Erro ao cadastrar aluno!");
+    }
+}
+
+
+function listarAlunos() {
+    const output = document.getElementById("output");
+    output.innerHTML = "";
+
+    if (alunos.length === 0) {
+        output.innerHTML = "Nenhum aluno cadastrado.";
+        return;
+    }
+
+    alunos.forEach((aluno, index) => {
+        output.innerHTML += `<p>Aluno ${index + 1}: ${aluno.nome}, Idade: ${aluno.idade}, Endereço: ${aluno.endereco}, Responsável: ${aluno.responsavel}</p>`;
+    });
+}
+
+
+
+
+
+
+
+
+
+function mostrarMensagem(msg) {
+    const output = document.getElementById("output");
+    output.innerHTML = `<p>${msg}</p>`;
+  }
